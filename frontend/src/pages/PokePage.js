702658499472273e76b191acfc79747120c04eb0
AlistairFR <jbwinckel62370@gmail.com>
2023-10-04 13:22:37 +0200

    ADD simple data to PokePage.js

--- frontend/src/pages/PokePage.js
+++ frontend/src/pages/PokePage.js
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function PokePage() {
-    // Récupérer l'id du pokemon
-    let { id } = useParams()
-    useEffect(() => {
-        // Récupérer les données du pokemon
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }, [id])
+  let { id } = useParams();
+  const [pokemonData, setPokemonData] = useState(null);
 
-    return (
-    <div>PokePage</div>
-    )
+  useEffect(() => {
+    // Récupérer les données du pokemon
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // Stocker les données dans l'état local
+        setPokemonData(data);
+      });
+  }, [id]);
+
+  return (
+    <div>
+        <h1 className='text-capitalize'>{pokemonData ? pokemonData.name : 'Chargement...'}</h1>
+        {pokemonData && (
+            <img
+                src={pokemonData.sprites.front_default}
+                alt={`Sprite de ${pokemonData.name}`}
+            />
+        )}
+    </div>
+  );
 }
 
-export default PokePage
+export default PokePage;
